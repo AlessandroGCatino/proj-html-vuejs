386b52bd91933b29073b95ccd15148a4460e5cb5
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-20 16:39:40 +0100

    creazione files e componenti base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-
+    <HeaderMenu/>
+    <MainContent/>
+    <FooterMenu/>
 </template>
 
 <script>
     import axios from 'axios'
     import { store } from './store'
+    import HeaderMenu from "./components/HeaderMenu.vue"
+    import FooterMenu from "./components/FooterMenu.vue"
+    import MainContent from "./components/MainContent.vue"
     
     export default {
+        components:{
+            HeaderMenu,
+            FooterMenu,
+            MainContent
+        },
         data() {
             return{
                 store,
---
+++ src/components/FooterMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterMenu",
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
