2b7f4b92036d94aee0a16ee187c7c0cf0a3e14da
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-20 17:53:20 +0100

    conclusione nav header

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
+        <div class="data">
+            <div class="container phone">
+                <small>We have a 95% Successful Pass Rate!</small>
+                <small>
+                    <i class="fa-solid fa-phone-flip"></i>
+                    <span>Give us a call to book your tuition! 1-800-555-555</span>
+                </small>
+            </div>
+        </div>
+        <HeroNav/>
+
     </div>
 </template>
 
 <script>
+import HeroNav from "./HeroNav.vue"
+
 export default {
     name: "HeaderMenu",
+    components: {
+        HeroNav
+    },
     data () {
         return {
 
@@ -15,5 +31,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+    .data{
+        background-color: $dark-gray;
+        padding: 10px;
+        .container.phone{
+            color: $gray;
+            display: flex;
+            justify-content: space-between;
+            i{
+                margin-right: 10px;
+            }
+        }
+    }
 
+    
 </style>
---
+++ src/components/HeroNav.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="hero">
+        <div class="hero-nav">
+            <div class="container">
+                <nav>
+                    <figure>
+                        <img src="../assets/img/avada-drivers-logo-2x.png" alt="logo">
+                    </figure>
+                    <div class="d-flex">
+                        <ul>
+                            <li>home</li>
+                            <li>about</li>
+                            <li>prices</li>
+                            <li>courses <span class="badge">New</span></li>
+                            <li>locations</li>
+                            <li>blog</li>
+                            
+                        </ul>
+                        <span class="btn btn-success">book now</span>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="container testo">
+            <h1>
+                Drive with Avada
+            </h1>
+            <h5>
+                We offer the finest driving tuition money can buy
+            </h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroNav",
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+
+.btn{
+    background-color: $green;
+    border: $green;
+    padding: 6px 20px;
+    border-radius: 30px;
+    margin-left: 30px;
+}
+.hero{
+    background-image: url("../assets/img/homepage-hero-background.jpg");
+    background-position: center;
+    background-size: cover;
+    .hero-nav{
+        background-color: rgba(50, 50, 50, 0.5);
+        padding: 20px;
+        .container{
+            nav{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            text-transform: uppercase;
+            color: white;
+            ul{
+                display: flex;
+                gap:30px;
+                margin: 0;
+                li{
+                    list-style-type: none;
+                    position: relative;
+                    padding: 6px 12px;
+                    display: flex;
+                    align-items: center;
+                    span{
+                        background-color: $green;
+                        margin-left: 5px;
+                    }
+                    &:hover{
+                        color: $green;
+                        &::after{
+
+                            position: absolute;
+                            content: "";
+                            width: 80%;
+                            left: 10%;
+                            bottom: -5px;
+                            border-bottom: 3px solid $green;
+                            }
+                        }
+                    }
+                }
+            }
+                    
+            figure{
+                margin: 0;
+                img{
+                    display: block;
+                    width: 300px;
+                }
+            }
+        }
+    }
+}
+.container.testo{
+    color: white;
+    padding-block: 200px;
+}
+
+</style>
